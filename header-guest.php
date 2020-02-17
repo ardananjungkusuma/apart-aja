eb3fa66df8e819eebe720e47675663e0828060bb
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-02-17 17:42:07 +0700

    Commit for backup

---
+++ header-guest.php
@@ -0,0 +1,51 @@
+<header>
+    <nav class="navbar navbar-dark" style="background-color: white;padding-left:20px;box-shadow: 0px 1px rgba(0, 0, 0, 0.2);">
+        <style>
+            body {
+                font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            }
+
+            a {
+                color: black;
+            }
+
+            a:hover {
+                color: black;
+                text-decoration: none;
+            }
+
+            .menu-navbar {
+                position: absolute;
+                margin-left: 200px;
+                margin-top: 15px;
+            }
+
+            ul,
+            li {
+                display: inline-block;
+                list-style: none;
+                font-size: 20px;
+                margin-left: 5px;
+                padding-bottom: 5px;
+            }
+        </style>
+        <a href="index.php" style="display: inline">
+            <img src="assets/img/logo/logoapartaja.jpg" alt="Logo" style="width: 160px;height:40px;margin:5px;float:left">
+        </a>
+        <div class="menu-navbar">
+            <ul>
+                <li>
+                    <a href="a">Home</a>
+                </li>
+                <li>
+                    <a href="b">Apartement</a>
+                </li>
+            </ul>
+        </div>
+        </a>
+        <div class="auth" style="font-size: 20px;">
+            <a href="login.php">Login</a> |
+            <a href="signup.php">Sign Up</a>
+        </div>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,2 +1,68 @@
-<?php
-echo "This is index page";
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Apart Aja</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <style>
+        .parallax {
+            /* The image used */
+            background-image: url('assets/img/etc/scrollimg1.jpg');
+
+            /* Set a specific height */
+            min-height: 300px;
+
+            /* Create the parallax scrolling effect */
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .button-center {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            background-color: #555;
+            color: white;
+            font-size: 16px;
+            padding: 12px 24px;
+            border: none;
+            cursor: pointer;
+            border-radius: 5px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    include 'header-guest.php';
+    ?>
+    <div class="container" style="padding: 20px; margin: 10 px auto; margin-left: auto; margin-right: auto;">
+        <img src="assets/img/etc/header-web.jpg" alt="header"">
+    </div>
+
+    </div>
+    <div class=" container" style="padding: 20px; margin: 10 px auto; margin-left: auto; margin-right: auto;">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Kategori</h1>
+                <hr>
+                <br><br><br><br><br><br><br><br><br><br>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="padding: 20px; margin: 10 px auto; margin-left: auto; margin-right: auto;">
+        <div class="parallax">
+        </div>
+    </div>
+</body>
+
+</html>
