c0e2f6e4a2ff49422e31dd8f7384ae49bacbbf9a
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-02-25 10:34:00 +0700

    upload some files

---
+++ manage-apartement.php
@@ -0,0 +1,22 @@
+<?php include 'pengelola-sidebar.php' ?>
+<!-- Page Content -->
+<div id="page-content-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <img src="assets/img/logo/logoapartaja.jpg" style="width:160px; height:40px;display:inline;">
+                <h4 style="display: inline;float:right">Welcome </h4>
+            </div>
+            <div class="col-lg-12">
+                Manage Your Apartement's
+                <!-- last here -->
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+</body>
+
+</html>
